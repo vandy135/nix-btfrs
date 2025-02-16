5c8b78378433954efa479fabf50e0876343c6cef
vandy135 <110067885+vandy135@users.noreply.github.com>
2025-02-16 02:37:02 +0000

    Update btfrs-setup.sh

--- btfrs-setup.sh
+++ btfrs-setup.sh
@@ -1,37 +1,35 @@
-#!/bin/bash
+!/bin/bash
 
-nix-shell -p cryptsetup
-
-install_drive="/dev/nvme0n1"
+#validate drive
+#install_drive="/dev/nvme0n1"
 
 # Zap the NVMe drive
-sudo sgdisk --zap-all "$install_drive"
-
+sgdisk --zap-all "$install_drive"
 # Create a 1GB boot partition
-sudo sgdisk -n 1:0:+1G -t 1:EF00 "$install_drive"
-
+sgdisk -n 1:0:+1G -t 1:EF00 "$install_drive"
 # Create a partition for the remaining space
-sudo sgdisk -n 2:0:0 -t 2:BF00 "$install_drive"
-
+sgdisk -n 2:0:0 -t 2:BF00 "$install_drive"
 # Make sure Boot is bootable
-sudo parted $install_drive -- set 1 esp on
+parted $install_drive -- set 1 esp on
 
-sudo mkfs.fat -F 32 /dev/nvme0n1p1
-sudo fatlabel /dev/nvme0n1p1 BOOT
+mkfs.fat -F 32 /dev/nvme0n1p1
+fatlabel /dev/nvme0n1p1 BOOT
 
-sudo cryptsetup --batch-mode -c aes-xts-plain64 --use-random luksFormat "/dev/nvme0n1p2"
-sudo cryptsetup luksOpen "/dev/nvme0n1p2" luks
+cryptsetup --batch-mode -c aes-xts-plain64 --use-random luksFormat "/dev/nvme0n1p2"
+cryptsetup luksOpen "/dev/nvme0n1p2" luks
 
-sudo mkfs.btrfs /dev/mapper/luks
-#sudo mkdir /mnt
-sudo mount /dev/mapper/luks /mnt
-sudo btrfs subvolume create /mnt/root
-sudo btrfs subvolume create /mnt/home
-sudo btrfs subvolume create /mnt/persist
-sudo umount /mnt
-sudo mount -o subvol=root,compress=lzo /dev/mapper/luks /mnt
-sudo mkdir /mnt/{boot,home,persist}
-sudo mount -o subvol=home,compress=lzo /dev/mapper/luks /mnt/home
-sudo mount -o subvol=persist,compress=lzo /dev/mapper/luks /mnt/persist
-sudo mount /dev/nvme0n1p1 /mnt/boot
+mkfs.btrfs /dev/mapper/luks
+# mkdir /mnt # do not need with install media
+mount /dev/mapper/luks /mnt
+btrfs subvolume create /mnt/root
+btrfs subvolume create /mnt/home
+btrfs subvolume create /mnt/persist
+umount /mnt
+mount -o subvol=root,compress=lzo /dev/mapper/luks /mnt
+mkdir /mnt/{boot,home,persist}
+mount -o subvol=home,compress=lzo /dev/mapper/luks /mnt/home
+mount -o subvol=persist,compress=lzo /dev/mapper/luks /mnt/persist
+mount /dev/nvme0n1p1 /mnt/boot
 
+nixos-generate-config --root /mnt
+nixos-install
